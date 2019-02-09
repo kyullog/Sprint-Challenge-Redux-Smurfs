43461bec36ba0ca168f72e28b4db7a6062b81219
Jonathan Dorety <kyullog@gmail.com>
2019-02-09 14:35:39 -0500

    Smurfs!

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -5,6 +5,7 @@ import Smurf from "./Smurf";
 const SmurfList = props => {
   return (
     <div className="smurf-list">
+      <h1>Smurfs!</h1>
       {props.smurfs.map(smurf => (
         <Smurf key={smurf.id} smurf={smurf} />
       ))}
